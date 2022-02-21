17c13ddbb95ecf2bb52f59feef42590ee63b2e88
riftz87 <riftz.dev@gmail.com>
2022-02-21 18:52:58 +0800

    first commit

---
+++ bin/cli.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+    console.log('You have to provide a name to your block.');
+    console.log('For example :');
+    console.log('    npx create-my-boilerplate my-block');
+    process.exit(1);
+}
+
+const projectName = process.argv[2];
+const currentPath = process.cwd();
+const projectPath = path.join(currentPath, projectName);
+const git_repo = '';
+
+try {
+    fs.mkdirSync(projectPath);
+} catch (err) {
+    if (err.code === 'EEXIST') {
+      console.log(`The file ${projectName} already exist in the current directory, please give it another name.`);
+    } else {
+      console.log(error);
+    }
+    process.exit(1);
+}
+
+async function main() {
+    try {
+      console.log('Downloading files...');
+      execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
+
+      process.chdir(projectPath);
+
+      console.log('Installing dependencies...');
+      execSync('npm install');
+    } catch (error) {
+      console.log(error);
+    }
+}
+main();
